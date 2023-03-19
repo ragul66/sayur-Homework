04a5c6a60f5a8cdc8302e530602b99f347efb25a
ragul66 <ragulvasanth24@gmail.com>
2023-03-19 23:31:41 +0530

    cafe dict

---
+++ dictionary/cafe.py
@@ -0,0 +1,39 @@
+'''1. Write an app for a cafe. Decide on the items in the cafe, stock of each item in the morning, 
+stock in the evening, sales amount at the end of the day and profit for each item. You need to restock an item 
+if the supply reaches 20% of the stock. Print the 3 items with highest sales, and top 3 highest profit.'''
+
+
+item={
+    'tea':{
+    'name':'tea',
+    'price':30,
+    'stock':25,
+    'refill':25,
+    'profit':10,
+    'sales':0
+    },
+    'coffee':{
+    'name':'coffee',
+    'price':25,
+    'stock':20,
+    'refill':20,
+    'profit':5,
+    'sales':0
+    },
+    'cookies':{
+    'name':'cookies',
+    'price':15,
+    'stock':35,
+    'refill':35,
+    'profit':5,
+    'sales':0
+    },
+    'capppucino':{
+    'name':'cappucino',
+    'price':23,
+    'stock':19,
+    'refill':19,
+    'profit':5,
+    'sales':0
+}
+}
