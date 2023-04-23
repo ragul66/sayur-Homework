1285f8337cc5b810cabdd47fb4f59ddae3c00b52
ragul66 <ragulvasanth24@gmail.com>
2023-04-23 18:40:01 +0530

    bowling game

--- list/divisible sums/countupperlower.py
+++ list/divisible sums/countupperlower.py
@@ -1,11 +1,11 @@
 
 '''Write a Python function that accepts a string and counts the number of upper and lower case letters.'''
 
-def count_upper_lower(string):
+def count_upper_lower(instring):
     upper_count = 0
     lower_count = 0
 
-    for char in string:
+    for char in instring:
         if char.isupper():
             upper_count += 1
         elif char.islower():
@@ -14,7 +14,7 @@ def count_upper_lower(string):
     print("Number of uppercase letters:", upper_count)
     print("Number of lowercase letters:", lower_count)
     
-string=input("Enter the String")
-count_upper_lower()
+instring=input("Enter the String")
+count_upper_lower(instring)
 
 
